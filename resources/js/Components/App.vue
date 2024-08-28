49ad678c3b739d63be067637ec0f9abd8fad83f9
Mesut <kircilm@gmail.com>
2024-08-28 22:58:13 +0200

    initialisation du projet

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="p-4 bg-gray-100 text-center">
+        <h1 class="text-2xl font-bold text-blue-600">Bienvenue sur Laravel avec Vue.js</h1>
+        <p class="text-gray-700">Ceci est un composant Vue intégré dans Laravel.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #3490dc;
+}
+</style>
